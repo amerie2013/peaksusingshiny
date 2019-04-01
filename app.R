ecb7e5b3f0ddb93984730eac9db5d50cc95ff5a9
Ahmed Merie <38012129+amerie2013@users.noreply.github.com>
2019-04-01 19:49:44 -0400

    Add files via upload

--- app.R
+++ app.R
@@ -1,50 +1,79 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
-
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
-)
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+library(dplyr)
+peak <- function(x){
+  order_end   <- 1
+  n <- 41
+  order_end   <- 1
+  start  <- rep(0, n)
+  height   <- rep(0, n)
+  end    <- rep(0, n)
+  kind   <- rep(0, n)
+  peaks <- data.frame(start,height, end, kind)
+  for (j in 1:n) {
+    for (i in order_end:(length(x)-5)){
+      if (x[i+5]-x[i]>10){
+        peaks$start[j] = (x[i])
+        order_start = i
+        break
+      }
+    }
+    for (i in order_start:(length(x)-5)){
+      if ((x[i]>x[i+1]) & (x[i]>x[i+2]) & (x[i]>x[i+3]) & (x[i]>x[i+4]) & (x[i]>x[i+5])){
+        peaks$height[j] = x[i]
+        order_high = i
+        break
+      }
+    }
+    for (i in order_high:(length(x)-5)){
+      if (x[i+5]-x[i]>2){
+        peaks$end[j] = (x[i])
+        order_end = i
+        break
+      }
+    }
+    if (peaks$height[j]-peaks$start[j] > 200){
+      peaks$kind[j]="HIGH PEAK"
+    }
+    else{
+      peaks$kind[j]="MEDIUM PEAK"
+    }
+  }
+  return(peaks)
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      fileInput("file1", "Choose CSV File"),
+      selectInput("col", "Select a column", character(0))
+    ),
+    mainPanel(
+      tableOutput("table"),
+      textOutput("selected")
+    )
+  )
+)
 
+server <- function(input, output, session) {
+  data <- reactive({
+    inFile <- input$file1
+    if (is.null(inFile)) return(NULL)
+    read.csv(inFile$datapath)
+  })
+  
+  observeEvent(data(), {
+    updateSelectInput(session, "col", choices = names(data()))
+  })
+  
+  output$table <- renderTable({
+    req(data())
+    head(data())
+  })
+  
+  output$table <- renderTable({
+    req(data())
+    peak(data()[[input$col]])
+  })
+}
+
+shinyApp(ui, server)
